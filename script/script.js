3d4db72885cce6179350a85a6c6f245e9f544dda
Jordan <jordanlindgren@live.com>
2021-09-20 16:01:18 -1000

    added styling and finished generator

--- script/script.js
+++ script/script.js
@@ -16,7 +16,33 @@ function addPassword(){
          console.log("Invalid Length")
          return "Invalid length...Please enter  length 8-128"
      }
-
+    var userChoice = []
     var confirmNumbers = confirm("Do you like to have numbers? ")
+    var confirmUpperCase = confirm("Do you like to have Uppercase letters? ")
+    var confirmLowerCase = confirm("Do you like to have Lowercase letters? ")
+    var confirmCharacters = confirm("Do you like to have Special Characters? ")
+    if (!confirmNumbers && !confirmUpperCase && !confirmLowerCase && !confirmCharacters){
+        return "please choose at least one option"
+    }
+    if (confirmCharacters){
+        userChoice += validCharacters // userChoice = userChoice + validCharacters
+    }
+    if (confirmNumbers){
+        userChoice += validNumbers
+    }
+    if (confirmUpperCase){
+        userChoice += validUpperCase
+    }
+    if (confirmLowerCase){
+        userChoice += validLowerCase
+    }
+    console.log(userChoice)
+    var newPassword = ""
+    for(let i=0;i<passwordLength;i++){
+        var ind = Math.floor(Math.random() * userChoice.length)
+        newPassword +=userChoice[ind]
+    }
+    console.log(newPassword)
+    return newPassword
 }
 genButton.addEventListener("click",generatePassword)
