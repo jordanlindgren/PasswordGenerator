208189e922e028a6835c98ae675b15f5b842601b
Jordan <jordanlindgren@live.com>
2021-09-20 15:41:53 -1000

    length validation complete

--- script/script.js
+++ script/script.js
@@ -1,8 +1,22 @@
 var genButton = document.getElementById("genPassword")
 var genInput = document.getElementById("password")
-
+var validNumbers = "1234567890"
+var validUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var validLowerCase = "abcdefghijklmnopqrstuvwxyz"
+var validCharacters = "!@#$%^&*()-="
 function generatePassword(){
     console.log("Btn click")
+    genInput.innerText = addPassword()
 }
+function addPassword(){
+    
+    var passwordLength = parseInt(prompt("Enter length of the password"))
+    console.log(passwordLength)
+     if (passwordLength < 8 || passwordLength > 128){
+         console.log("Invalid Length")
+         return "Invalid length...Please enter  length 8-128"
+     }
 
+    var confirmNumbers = confirm("Do you like to have numbers? ")
+}
 genButton.addEventListener("click",generatePassword)
