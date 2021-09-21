4a1974ab60058b9566f9b369f708c0695338bba0
Jordan <jordanlindgren@live.com>
2021-09-20 15:22:42 -1000

    intial add

---
+++ script/script.js
@@ -0,0 +1,8 @@
+var genButton = document.getElementById("genPassword")
+var genInput = document.getElementById("password")
+
+function generatePassword(){
+    console.log("Btn click")
+}
+
+genButton.addEventListener("click",generatePassword)
